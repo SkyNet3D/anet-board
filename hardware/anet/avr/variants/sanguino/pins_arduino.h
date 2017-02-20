efbce07d86be07cb3795159f7b6e253d2ca66f48
Hans Raaf <hara@oderwat.de>
2017-02-20 01:18:50 +0100

    Added pin to interrupt macro (thanks for the headsup Lenny)

--- hardware/anet/avr/variants/sanguino/pins_arduino.h
+++ hardware/anet/avr/variants/sanguino/pins_arduino.h
@@ -135,6 +135,9 @@ static const uint8_t A7 = 24;
                                 ( (((p) >=  8) && ((p) <= 15)) ? ((p) - 8) : \
                                 0 ) ) ) )
 
+#define digitalPinToInterrupt(p) ((p) == 10 ? 0 : ((p) == 11 ? 1 : ((p) == 2 ? 2 \
+                                : NOT_AN_INTERRUPT)))	// mega1284p dip
+
 #define PA 1
 #define PB 2
 #define PC 3
